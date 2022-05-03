2eead614231b68b8b81a71b4fc6053b6bb708e8b
suvhotta <suvhotta@gmail.com>
2022-05-03 14:30:35 +0530

    Dev: Implement soft delete

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class SoftDeleteManager(models.Manager):
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=False)
+
+
+class SoftDeleteBaseModel(models.Model):
+
+    is_deleted = models.BooleanField(default=False)
+    objects = SoftDeleteManager()
+
+    def soft_delete(self):
+        self.is_deleted = True
+        self.save()
+
+    def restore(self):
+        self.is_deleted = False
+        self.save()
+
+    class Meta:
+        abstract = True
